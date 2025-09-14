266e72d6cd08624c4aef8a31bf31277a32d8b037
Patil Rameshwar <rameshwarr.p001@gmail.com>
2025-09-14 21:10:23 +0530

    Day 1

--- App.jsx
+++ App.jsx
@@ -1,10 +1,65 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+// // text display
 
-export default function App() {
+// // import React from "react";
+// import { View, Text, StyleSheet ,Image} from "react-native";
+
+// const App = () => {
+//   return (
+//     <View>
+//       <Text >done  </Text>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+
+// });
+
+// export default App;
+
+//=======================================================//===============================//====================================
+
+// // image from url
+
+// import { StyleSheet, View, Image, Text } from 'react-native'
+
+// const App = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Image 
+//         style={{ width: 450, height: 1000 }}
+//         source={{ uri: "https://i.pinimg.com/736x/6e/fd/46/6efd46d9e838154cc665a51334ebfcdf.jpg" }}
+//       />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+// });
+
+// export default App;
+
+//=======================================================//===============================//====================================
+
+
+import { StyleSheet, Text, View ,Button, TouchableOpacity} from 'react-native'
+import React from 'react'
+
+const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello World</Text>
+    <View>
+      {/* <Button title='Press Me'></Button> */}
+
+      <TouchableOpacity>
+        <Text>Press Me</Text>`
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+
+
+const styles = StyleSheet.create({
+  
+});
+export default App;
